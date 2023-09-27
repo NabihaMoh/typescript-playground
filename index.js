fa7a431c58140f0c253bb508ebdaa07a3016efee
jeyjeycodes <83839350+jeylanicodes@users.noreply.github.com>
2023-09-27 23:09:47 +0100

    added example code

---
+++ index.js
@@ -0,0 +1,24 @@
+let age = 21;
+
+age = "mame";
+
+age > 20 ? console.log('IT is greater then 20') : console.log('IT is less then 20');
+
+const names = [];
+
+for(let i; i < 10; i++) {
+
+}
+
+for(x in names) {
+}
+
+function getListOfNames() {
+
+}
+getListOfNames();
+
+const getNewList = () => {
+
+};
+
