4de30560a0d51e3ce8237c4f4b6e6e0c749d4436
jeyjeycodes <83839350+jeylanicodes@users.noreply.github.com>
2023-09-28 01:46:54 +0100

    added typescript to project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+console.log("TEST TYPESCRIPT FILE");
+let age = 21;
+
+age > 20
+  ? console.log("IT is greater then 20")
+  : console.log("IT is less then 20");
+
+const names = [];
+
+for (let i; i < 10; i++) {}
+
+function getListOfNames() {}
+getListOfNames();
+
+const getNewList = () => {};
+
+// TODO
